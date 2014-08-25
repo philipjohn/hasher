1e48e1561441696e7534c32d3035ee80c7cbfd89
Philip John <phil@philipjohn.co.uk>
2014-08-25 14:23:39 +0100

    Library dependency

--- hasher.php
+++ hasher.php
@@ -6,4 +6,18 @@
  * Version: 0.1
  * Author: Philip John
  * Author URI: http://philipjohn.me.uk
+ * Depends: lib-twitter-api
  */
+
+/**
+ * Require the Twitter API plugin
+ */
+function hasher_init() {
+
+	// Check for the Dependencies plugin
+	if ( ! is_plugin_active( 'wp-plugin-dependencies/plugin-dependencies.php' ) )
+		die( __('Oops, you don\'t have the <a href="https://github.com/x-team/wp-plugin-dependencies">WP Dependencies Plugin</a> installed!') );
+
+}
+
+register_activation_hook( __FILE__, 'hasher_init' );
