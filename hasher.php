48f1fac3efc48588d086c1e772f6a7b601442216
Philip John <phil@philipjohn.co.uk>
2014-08-25 13:06:45 +0100

    Plugin base

---
+++ hasher.php
@@ -0,0 +1,9 @@
+<?php
+/*
+ * Plugin Name: Hasher
+ * Description: Monitor and record Twitter hashtags using WordPress
+ * Plugin URI: http://github.com/philipjohn/hasher.git
+ * Version: 0.1
+ * Author: Philip John
+ * Author URI: http://philipjohn.me.uk
+ */
